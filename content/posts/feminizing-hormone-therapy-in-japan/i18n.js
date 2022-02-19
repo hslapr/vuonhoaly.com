69d4f2e1001d790e4349bafd48a1f10ab4b1dc3d
hslapr <hslapr@gmail.com>
2022-02-19 17:00:05 +0900

    update

--- content/posts/feminizing-hormone-therapy-in-japan/i18n.js
+++ content/posts/feminizing-hormone-therapy-in-japan/i18n.js
@@ -2,19 +2,32 @@ var en2ja = {
   "estradiol": "エストラジオール",
   "conjugated estrogen": "結合型エストロゲン",
   "estradiol valerate": "エストラジオール吉草酸エステル",
-  "medroxyprogesterone acetate": "メドロキシプロゲステロン酢酸エステル"
+  "medroxyprogesterone acetate": "メドロキシプロゲステロン酢酸エステル",
+  "provera": "プロベラ",
+  "climara": "クリマラ",
+  "oestrogel": "オエストロジェル",
+  "premon": "プレモン",
+  "premarin": "プレマリン",
+  "progynova": "プロギノバ",
+  "malefe mtf": "マレフェMTF",
+  "meprate": "プロベラジェネリック",
+  "oestrodose": "オエストロドースジェル",
+  "estrofem": "エストロフェム",
+  "dermestril septem": "ダーメストリルセプテム",
+  "siterone": "シテロン",
+  "androcur": "アンドロクール"
 }
 
 var i18nElements = document.querySelectorAll(".i18n");
 for (let element of i18nElements) {
-  element.en = element.textContent;
+  element.title = element.textContent;
 }
 
 var language = "en";
 var languageForm = document.querySelector("#language-form");
 var radios = document.querySelectorAll("#language-form input[type=radio]");
 for (let radio of radios) {
-  radio.addEventListener('change', function(e) {
+  radio.addEventListener('change', function (e) {
     language = this.value;
     i18n();
   });
@@ -27,7 +40,8 @@ function i18n() {
     dict = en2ja;
   } else if (language == "en") {
     for (const e of i18nElements) {
-      e.textContent = e.en;
+      e.textContent = e.title;
+      e.lang = 'en';
     }
     return;
   }
@@ -35,6 +49,7 @@ function i18n() {
     let t = e.textContent.toLowerCase();
     if (t in en2ja) {
       e.textContent = dict[t];
+      e.lang = 'ja';
     }
   }
 }
